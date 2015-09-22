4b832e97bd23ca2f7bd8f7732a08aa01f8a1a175
Stephen Lien Harrell <sharrell@purdue.edu>
2015-09-22 15:30:08 -0400

    First pass at database and placesAPI

---
+++ app/src/main/java/com/example/cnitz/eatthis/DownloadRestaurants.java
@@ -0,0 +1,47 @@
+package com.example.cnitz.eatthis;
+
+import java.util.List;
+import java.util.ListIterator;
+import se.walkercrou.places.GooglePlaces;
+import se.walkercrou.places.Place;
+import se.walkercrou.places.Param;
+import com.example.cnitz.eatthis.RestaurantDb;
+
+/**
+ * Created by sharrell on 9/21/15.
+
+ */
+
+public class DownloadRestaurants {
+
+	private static String GPAPIKEY = "";
+	// Approx middle of campus: 40.425408, -86.919722
+	private static double lat = 40.425408;
+	private static double lng = -86.919722;
+	// most "campus" restaurants are within 2km  
+	private static int radius = 2000;
+	
+
+	private List<Place> restaurantList;
+
+	public void GrabRestaurantList() {
+
+		GooglePlaces client = new GooglePlaces(GPAPIKEY);
+
+		restaurantList = client.getNearbyPlaces(lat, lng, radius, GooglePlaces.MAXIMUM_RESULTS, Param.name("type").value("restaurant"));
+	}
+
+	
+	public void AddResturauntListToDatabase() {
+		RestaurantDb db = new RestaurantDb();
+		ListIterator<Place> iter = restaurantList.listIterator();
+		while(iter.hasNext()) {
+			Place currentPlace = iter.next();
+			db.InsertRestaurant(currentPlace.getPlaceId(), currentPlace.getName());
+
+		}
+			
+	}	
+
+	
+}
---
+++ app/src/main/java/com/example/cnitz/eatthis/RestaurantDb.java
@@ -0,0 +1,69 @@
+package com.example.cnitz.eatthis;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+import android.content.Context;
+import android.content.ContentValues;
+
+/**
+ * Created by sharrell on 9/21/15.
+ */
+
+public final class RestaurantDb {
+
+	private SQLiteDatabase writableDatabase = null; 
+	private SQLiteDatabase readableDatabase = null; 
+
+	private static final String SQL_CREATE_ENTRIES = 
+		"CREATE TABLE" + RestaurantEntry.TABLE_NAME + " (" + 
+		RestaurantEntry._ID + " INTEGER PRIMARY KEY," + 
+		RestaurantEntry.COLUMN_NAME_RESTAURANT_ID + " TEXT," +
+		RestaurantEntry.COLUMN_NAME_NAME + " TEXT" +
+		" )";
+
+	private static final String SQL_DELETE_ENTRIES =
+		"DROP TABLE IF EXISTS " + RestaurantEntry.TABLE_NAME;
+
+	public RestaurantDb() {}
+
+	public abstract class RestaurantEntry implements BaseColumns {
+		public static final String TABLE_NAME = "restaurant";
+		public static final String COLUMN_NAME_RESTAURANT_ID = "restaurantid";
+		public static final String COLUMN_NAME_NAME = "name";
+	}
+
+	public class RestaurantDbHelper extends SQLiteOpenHelper {
+		// If you change the database schema, you must increment the database version.
+		public static final int DATABASE_VERSION = 1;
+		public static final String DATABASE_NAME = "Restaurant.db";
+
+		public RestaurantDbHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+		public void onCreate(SQLiteDatabase db) {
+			db.execSQL(SQL_CREATE_ENTRIES);
+		}
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			db.execSQL(SQL_DELETE_ENTRIES);
+			onCreate(db);
+		}
+	}
+
+	public void InsertRestaurant(String id, String name) {
+		RestaurantDbHelper dbHelper;
+		if(writableDatabase == null) {
+			dbHelper = new RestaurantDbHelper(getContext());
+			writableDatabase = dbHelper.getWritableDatabase();
+		}
+		ContentValues values = new ContentValues();
+		values.put(RestaurantEntry.COLUMN_NAME_RESTAURANT_ID, id);
+		values.put(RestaurantEntry.COLUMN_NAME_NAME, name);
+
+		long newRowId = writableDatabase.insert(RestaurantEntry.TABLE_NAME,
+			 null, values);
+		
+	}
+		
+}
+
