6c227eaaf48a1bee80cb335da69ef3cc6a239efb
fische17 <fische17@purdue.edu>
2015-10-30 22:20:32 -0400

    Fixed the stuff again

--- app/src/main/java/com/example/cnitz/eatthis/ResterauntList.java
+++ app/src/main/java/com/example/cnitz/eatthis/ResterauntList.java
@@ -46,7 +46,12 @@ public class ResterauntList extends ActionBarActivity  implements RestaurantsLis
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        MapFragment fragment = new MapFragment();
+        transaction.replace(R.id.content_frame, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
 
 
         
@@ -164,7 +169,8 @@ public class ResterauntList extends ActionBarActivity  implements RestaurantsLis
         FragmentManager manager = getFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
         MapFragment fragment = new MapFragment();
-        transaction.add(R.id.content_frame, fragment);
+        transaction.replace(R.id.content_frame, fragment);
+        transaction.addToBackStack(null);
 
 
         transaction.commit();
